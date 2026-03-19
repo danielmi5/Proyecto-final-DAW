a852e0663753133c220b14a9d970849859412912
danielmi5 <midaniel@iesrafaelalberti.es>
2026-03-19 23:17:06 +0100

    feat(backend): creado proyecto Springboot backend.

---
+++ backend/src/test/java/com/estimplytics/backend/EstimplyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estimplytics.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimplyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
