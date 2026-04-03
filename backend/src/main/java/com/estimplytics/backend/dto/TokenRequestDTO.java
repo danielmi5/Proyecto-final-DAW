b64cbc6655b2afaf851622d3ebd043b4a45dd6df
danielmi5 <midaniel@iesrafaelalberti.es>
2026-04-03 09:11:42 +0200

    feat(backend): creados DTOs para request y response de tokens.

---
+++ backend/src/main/java/com/estimplytics/backend/dto/TokenRequestDTO.java
@@ -0,0 +1,15 @@
+package com.estimplytics.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenRequestDTO {
+    private String email;
+    private String password;
+}
