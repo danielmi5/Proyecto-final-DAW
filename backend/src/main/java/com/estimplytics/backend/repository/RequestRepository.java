b4fe4a3593682fef20fc43af6e605c040d1d3a89
danielmi5 <midaniel@iesrafaelalberti.es>
2026-04-17 11:21:27 +0200

    feat(backend): añadida búsqueda por redmineId en el repositorio para evitar duplicar requests.

--- backend/src/main/java/com/estimplytics/backend/repository/RequestRepository.java
+++ backend/src/main/java/com/estimplytics/backend/repository/RequestRepository.java
@@ -4,8 +4,10 @@ import com.estimplytics.backend.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, UUID> {
+    Optional<Request> findByRedmineId(Integer redmineId);
 }
